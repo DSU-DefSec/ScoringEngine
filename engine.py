6eb72474364e929e41d327f311485db009927515
nic <nems608@gmail.com>
2017-10-21 16:17:10 -0500

    Finish distributed polling

--- engine.py
+++ engine.py
@@ -7,8 +7,10 @@ import sys
 
 class ScoringEngine(object):
 
-    def __init__(self):
+    def __init__(self, team_num=None):
         self.dm = DataManager()
+        self.dm.teams.sort(key=lambda t: t.name)
+        self.team_num = team_num
 
     def start(self):
         self.check()
@@ -18,14 +20,18 @@ class ScoringEngine(object):
         while True:
             print("New Round of Checks")
             self.dm.reload_credentials()
-            self.dm.teams.sort(key=lambda t: t.name)
+
             current = time.monotonic()
             print(datetime.timedelta(seconds=current - start))
             if (current - start) > self.dm.settings["comp_length"]:
                 break
  
             for service in self.dm.services:
-                service.check(self.dm.teams)
+                if self.team_num is None:
+                    service.check(self.dm.teams)
+                else:
+                    service.check([self.dm.teams[team_num]])
+
             wait = self.dm.settings["interval"]
             print("Interval: " + str(wait))
             jitter = random.randint(-self.dm.settings["jitter"],
@@ -38,6 +44,11 @@ class ScoringEngine(object):
 
         
 if __name__ == '__main__':
-#    team_num = int(sys.argv[1])
-    engine = ScoringEngine()
+    if len(sys.argv) > 1:
+        print("Usage: ./engine [team_number]")
+    if len(sys.argv) == 0:
+        engine = ScoringEngine()
+    if len(sys.argv) == 1:
+        team_num = int(sys.argv[1])
+        engine = ScoringEngine(team_num)
     engine.start()
