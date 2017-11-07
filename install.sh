472f3ad599af190dc2167bd9f19220a363bfb6bc
Nic Mews-Schmuck <nems608@gmail.com>
2017-11-07 16:14:00 -0600

    Install script fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y mysql-server python3 python3-pip python-dev freetds-dev libssl-dev libffi-dev libldap2-dev libsasl2-dev freerdp
-sudo pip3 
+sudo pip3 install -U pip
 sudo pip3 install dnspython paramiko pysmb pymysql pymssql pyldap requests Flask
