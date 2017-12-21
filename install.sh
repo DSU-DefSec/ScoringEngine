cc4266cd8135255ddf41c61a8067b6bfb87a962b
nic <nems608@gmail.com>
2017-12-21 10:34:36 -0600

    Added new dependencies to installer

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y mysql-server python3 python3-pip python-dev freetds-dev libssl-dev libffi-dev libldap2-dev libsasl2-dev freerdp
+sudo apt-get install -y mysql-server python3 python3-pip python-dev python3-tk freetds-dev libssl-dev libffi-dev libldap2-dev libsasl2-dev freerdp
 sudo pip3 install -U pip
-sudo pip3 install dnspython paramiko pysmb pymysql pymssql pyldap requests Flask matplotlib
+sudo pip3 install dnspython paramiko pysmb pymysql pymssql pyldap requests Flask flask-login flask-wtf matplotlib bcrypt
