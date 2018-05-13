2b98e52ede984223666f80383413954cff88c7b7
Nic Mews-Schmuck <nems608@gmail.com>
2018-05-12 20:36:44 -0500

    rtd

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python
+
+setup(name='scoring',
+    version='0.1',
+    description="CCDC Scoring Engine",
+    long_description="",
+    author='Nic Mews-Schmuck',
+    author_email='nems608@gmail.com',
+    license='TODO',
+    packages=['scoring'],
+    zip_safe=False,
+    install_requires=[],
+)
