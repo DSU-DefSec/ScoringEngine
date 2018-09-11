2a2317c971dc3cadfb126dc28393849d66a02016
Nic Mews-Schmuck <nems608@gmail.com>
2018-09-11 08:37:43 -0500

    Got rid of pysmb since it sucks, replaced with smbclient

--- scoring/engine/polling/poll_smb.py
+++ scoring/engine/polling/poll_smb.py
@@ -1,12 +1,8 @@
-import smb
-from smb.base import *
-from smb.smb_structs import *
-from nmb.NetBIOS import NetBIOS
-from smb.SMBConnection import SMBConnection
 import socket
 
 from .poller import PollInput, PollResult
 from .file_poller import FilePoller
+import subprocess
 
 class SmbPollInput(PollInput):
 
@@ -28,23 +24,20 @@ class SmbPoller(FilePoller):
         username = poll_input.credentials.username
         password = poll_input.credentials.password
         domain = poll_input.credentials.domain
-    
+
+        share = '//{}/{}'.format(poll_input.server, poll_input.sharename)
+
+        extension = self.get_extension(poll_input.path)
+        f = self.open_file(extension)
+        f.close()
+        cmd = 'get {} {}'.format(poll_input.path, f.name)
+        smbcli = ['smbclient', '-U', username, share, password, '-c', cmd]
+        if not domain is None:
+            smbcli.extend(['-D', domain])
         try:
-            if domain is None:
-                conn = SMBConnection(username, password, '', poll_input.hostname)
-            else:
-                conn = SMBConnection(username, password, '', poll_input.hostname, domain.domain)
-
-            conn.connect(poll_input.server, poll_input.port)
-
-            extension = self.get_extension(poll_input.path)
-            f = self.open_file(extension)
-            conn.retrieveFile(poll_input.sharename, poll_input.path, f)
-            f.close()
-            conn.close()
-
+            subprocess.check_output(smbcli, stderr=subprocess.STDOUT)
             result = SmbPollResult(f.name)
             return result
         except Exception as e:
-            result = SmbPollResult(None, e)
+            result = SmbPollResult(None, e.output)
             return result
