ddc81e0ddc8d0523e6e179346525c5bbd0fe2fa0
Nic Mews-Schmuck <nems608@gmail.com>
2018-09-24 14:38:24 -0500

    Temp fix for DNS checks. Needs to allow for differences in answers based on teams. For now just check last octet

--- scoring/engine/checker/dns_check.py
+++ scoring/engine/checker/dns_check.py
@@ -3,5 +3,5 @@ def any_match(poll_result, expected):
     if poll_result.exception != "None":
         return False
 
-    host = poll_result.answer
+    host = poll_result.answer.split('.')[3]
     return host in expected
