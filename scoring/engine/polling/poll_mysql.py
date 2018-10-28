44f8733db1eff23d657b954c14d84f17296532b0
Nic Mews-Schmuck <nems608@gmail.com>
2018-10-28 15:48:45 -0500

    Use title case for poller names

--- scoring/engine/polling/poll_mysql.py
+++ scoring/engine/polling/poll_mysql.py
@@ -2,20 +2,20 @@ import pymysql
 
 from .poller import PollInput, PollResult, Poller
 
-class MySqlPollInput(PollInput):
+class MysqlPollInput(PollInput):
     
     def __init__(self, db, query, server=None, port=None):
-        super(MySqlPollInput, self).__init__(server, port)
+        super(MysqlPollInput, self).__init__(server, port)
         self.db = db
         self.query = query
 
-class MySqlPollResult(PollResult):
+class MysqlPollResult(PollResult):
 
     def __init__(self, output, exceptions=None):
-        super(MySqlPollResult, self).__init__(exceptions)
+        super(MysqlPollResult, self).__init__(exceptions)
         self.output = output
 
-class MySqlPoller(Poller):
+class MysqlPoller(Poller):
 
     def poll(self, poll_input):
         username = poll_input.credentials.username
