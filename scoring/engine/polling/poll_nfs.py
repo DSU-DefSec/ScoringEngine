3236d50736394005a95e2a351b52094585abd3dc
Nic Mews-Schmuck <nems608@gmail.com>
2019-01-04 18:37:21 +0000

    Add timeout to NFS poller, delay polling for each team so they happen sequentially. Concurrent NFS mounts cause issues

--- scoring/engine/polling/poll_nfs.py
+++ scoring/engine/polling/poll_nfs.py
@@ -1,6 +1,7 @@
 from .poller import PollInput, PollResult, Poller
 import os
 import subprocess
+import time
 
 class NfsPollInput(PollInput):
 
@@ -18,6 +19,7 @@ class NfsPollResult(PollResult):
 
 class NfsPoller(Poller):
     def poll(self, poll_input):
+        time.sleep((poll_input.team.id-1)*3)
         server = poll_input.server
         path = '/tmp/'+server
         file = poll_input.file
@@ -26,7 +28,7 @@ class NfsPoller(Poller):
                 os.mkdir(path)
             if (os.path.ismount(path)):
                 subprocess.call(['umount','-f',path])
-            subprocess.call(['mount','-t', 'nfs', server+':/',path])
+            subprocess.call(['mount.nfs', server+':/',path], timeout=2)
             with open(path+file, 'r') as f:
                 output = f.read()
             subprocess.call(['umount','-f',path])
