d6f228cfa1e68440c22698186f09ed704c6d7d26
Nic Mews-Schmuck <nems608@gmail.com>
2020-08-31 04:38:11 -0500

    Removed unused files

--- scripts/team.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for team in {1..4}; do
-    perc=$(mysql -u root -pPassword1! -e "SELECT SUM(password='Password1!' AND team_id=$team)/SUM(team_id=$team) AS perc FROM scoring.credential")
-    perc=$(echo $perc | cut -d ' ' -f 2)
-    date=$(date +%H:%M:%S)
-    echo $date,$perc >> /home/dsu/team_"$team"_default.csv
-done
